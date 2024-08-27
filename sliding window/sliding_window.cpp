d52b8e7716890e64a814267e620ac8a5d5dc978e
Aditya Kumar <aditya.kumar.mfp12@gmail.com>
2024-08-28 02:20:00 +0530

    Sliding Window

--- sliding window/sliding_window.cpp
+++ sliding window/sliding_window.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <limits.h>
 using namespace std;
 int main(){
-	cout<<"Hello";
+	int size;
+	cin>>size;
+	int arr[size];
+	for(int i = 0; i < size; i++){
+        cin>>arr[i];
+	}
+	int k;
+	cin>>k;
+	int sum = 0, max = INT_MIN, i = 0, j = 0;
+    while(j < size){
+         sum = sum + arr[j];
+         if((j - i + 1) < k){
+         	j++;
+         }else if((j - i + 1) == k){
+         	if(sum > max){
+         		max = sum;
+         	}
+         	sum = sum - arr[i];
+         	i++;
+         	j++;
+         }
+    }
+
+    cout<<max<<endl;
 	return 0;
 }
